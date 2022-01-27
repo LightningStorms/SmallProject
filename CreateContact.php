bc7e1299993e8578f8a35623d816954294f7924c
LoganBlair314 <97700682+LoganBlair314@users.noreply.github.com>
2022-01-27 18:04:22 -0500

    Add files via upload
    Initial file upload for Login, Registration, and CreateContact.

---
+++ CreateContact.php
@@ -0,0 +1,43 @@
+<?php
+
+$inData = getRequestInfo();
+
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$email = $inData["email"];
+$phone = $inData["phone"];
+$userId = $inData["userId"];
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "UserInfo");
+
+if( $conn->connect_error )
+{
+    returnWithError( $conn->connect_error );
+}
+else
+{
+    $stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Email, Phone, UserID) VALUES(?,?,?,?,?)");
+    $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $userId);
+    $stmt->execute();
+
+    $stmt->close();
+    $conn->close();
+
+    returnWithError("");
+}
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
